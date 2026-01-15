91ee8b080536443e3eb40bd8b51ddb2d1298f9f5
Franz Iacob <franz.iacob@cegeka.com>
2026-01-15 14:03:05 +0100

    chore: release 0.0.2 binary

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "SmartSIP-SDK-Libraries-and-examples",
+    name: "SmartSIP-SDK-Libraries-and-Examples",
     platforms: [.iOS(.v15)],
     products: [
         .library(name: "smartsip-sdk", targets: ["smartsip-sdk"])
@@ -21,8 +21,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "smartsip-sdk-binary",
-            url: "https://github.com/FranzIacobCegeka/SmartSIP-SDK-Libraries-and-examples/releases/download/0.0.2/smartsip-sdk-0.0.2.xcframework.zip",
-            checksum: "9e9f568e414d3c7d0ab7f418e5504c97fb66986bbc5400b68c19e2068bfa451f"
+            url: "https://github.com/FranzIacobCegeka/SmartSIP-SDK-Libraries-and-Examples/releases/download/0.0.2/smartsip-sdk-0.0.2.xcframework.zip",
+            checksum: "98b4c02401e061939dbf9bbcb2ed65760b06e10b8a1a490624b0d9c1bf9fccfc"
         )
     ]
 )
