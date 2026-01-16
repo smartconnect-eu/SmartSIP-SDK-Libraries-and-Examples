b27adc203ecb317c1be6714ff73e897c8ce9f4fd
Franz Iacob <franz.iacob@cegeka.com>
2026-01-16 14:52:16 +0100

    Release 0.0.26 - Fixed naming loop with SmartSipSDK target

--- Package.swift
+++ Package.swift
@@ -5,24 +5,24 @@ let package = Package(
     name: "SmartSIP-SDK-Libraries-and-Examples",
     platforms: [.iOS(.v15)],
     products: [
-        .library(name: "smartsip-sdk", targets: ["smartsip-sdk"])
+        .library(name: "SmartSipSDK", targets: ["SmartSipSDK"])
     ],
     dependencies: [
         .package(url: "https://gitlab.linphone.org/BC/public/linphone-sdk-swift-ios.git", exact: "5.4.73")
     ],
     targets: [
         .target(
-            name: "smartsip-sdk",
+            name: "SmartSipSDK",
             dependencies: [
                 .target(name: "smartsip-sdk-binary"),
                 .product(name: "linphonesw", package: "linphone-sdk-swift-ios")
             ],
-            path: "Sources/smartsip-sdk"
+            path: "Sources/SmartSipSDK"
         ),
         .binaryTarget(
             name: "smartsip-sdk-binary",
-            url: "https://github.com/smartconnect-eu/SmartSIP-SDK-Libraries-and-Examples/releases/download/0.0.22/smartsip-sdk-0.0.22.xcframework.zip",
-            checksum: "9136b6d462c06f4a53dde19ab8966ee38cf36379a29c6b6055a12a98e7ec0fdc"
+            url: "https://github.com/smartconnect-eu/SmartSIP-SDK-Libraries-and-Examples/releases/download/0.0.26/smartsip-sdk-0.0.26.xcframework.zip",
+            checksum: "29ca43481d3553761da3d387cb181fb3ffa12d004805a6db6a879eddd03bb645"
         )
     ]
 )
