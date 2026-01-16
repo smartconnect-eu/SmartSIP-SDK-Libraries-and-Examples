c0cffded8156aa0312b4afd7be92bedf48aab55e
Franz Iacob <franz.iacob@cegeka.com>
2026-01-16 13:25:38 +0100

    Release 0.0.17

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "smartsip-sdk-binary",
-            url: "https://github.com/smartconnect-eu/SmartSIP-SDK-Libraries-and-Examples/releases/download/0.0.6/smartsip-sdk-0.0.6.xcframework.zip",
-            checksum: "27c88eef2491747d42ec94befb415b1fd558e711478da85af05c410d600ac864"
+            url: "https://github.com/smartconnect-eu/SmartSIP-SDK-Libraries-and-Examples/releases/download/0.0.17/smartsip-sdk-0.0.17.xcframework.zip",
+            checksum: "189f1cd1971fc741b67f551723ae3a84f1f6c15ee56dc7f0814940304241b215"
         )
     ]
 )
