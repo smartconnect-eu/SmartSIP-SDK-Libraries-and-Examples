bc297977bc162f7e132f92ee976ed7be8f448810
Franz Iacob <franz.iacob@cegeka.com>
2026-01-16 15:19:09 +0100

    Release 0.0.32 - Using SmartSipSDK Namespace

--- Sources/SmartSipSDK/SmartSipSDK.swift
+++ Sources/SmartSipSDK/SmartSipSDK.swift
@@ -10,12 +10,18 @@ public enum CallState : String {
 }
 
 public final class SmartSipSDK {
-    public static let sdkVersion: String = "0.0.31"
+    public static let sdkVersion: String = "0.0.32"
     public static func setDelegate(_ delegate: CallDelegate) {}
     public typealias CallDestination = String
     public static func initialize(token: String, flowId: String, domain: String) {}
     public static func getCallDestinations() async -> [CallDestination] { return [] }
-    public static func makeCall(clientData: [String: Any]? = nil, destinationQueue: String? = nil, callerPhoneNumber: String? = nil, callerFullName: String? = nil, otherRoutingData: [String: Any]? = nil) async {}
+    public static func makeCall(
+        clientData: [String: Any]? = nil,
+        destinationQueue: String? = nil,
+        callerPhoneNumber: String? = nil,
+        callerFullName: String? = nil,
+        otherRoutingData: [String: Any]? = nil
+    ) async {}
     public static func hangUp() {}
     public static func setSIPDebugMode(enabled: Bool) {}
 }
