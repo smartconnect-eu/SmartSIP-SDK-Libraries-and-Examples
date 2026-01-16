2a63f7aa568f09496434eeebfc62ebcd325eac1a
Franz Iacob <franz.iacob@cegeka.com>
2026-01-16 15:05:22 +0100

    Release 0.0.28 - Unified module architecture

--- Package.swift
+++ Package.swift
@@ -5,24 +5,24 @@ let package = Package(
     name: "SmartSIP-SDK-Libraries-and-Examples",
     platforms: [.iOS(.v15)],
     products: [
-        .library(name: "smartsip-sdk", targets: ["smartsip-sdk"])
+        .library(name: "smartsip_sdk", targets: ["smartsip_sdk"])
     ],
     dependencies: [
         .package(url: "https://gitlab.linphone.org/BC/public/linphone-sdk-swift-ios.git", exact: "5.4.73")
     ],
     targets: [
         .target(
-            name: "smartsip-sdk",
+            name: "smartsip_sdk",
             dependencies: [
-                .target(name: "smartsip-sdk-binary"),
+                .target(name: "smartsip_sdk_binary_dist"),
                 .product(name: "linphonesw", package: "linphone-sdk-swift-ios")
             ],
-            path: "Sources/smartsip-sdk"
+            path: "Sources/smartsip_sdk"
         ),
         .binaryTarget(
-            name: "smartsip-sdk-binary",
-            url: "https://github.com/smartconnect-eu/SmartSIP-SDK-Libraries-and-Examples/releases/download/0.0.22/smartsip-sdk-0.0.22.xcframework.zip",
-            checksum: "9136b6d462c06f4a53dde19ab8966ee38cf36379a29c6b6055a12a98e7ec0fdc"
+            name: "smartsip_sdk_binary_dist",
+            url: "https://github.com/smartconnect-eu/SmartSIP-SDK-Libraries-and-Examples/releases/download/0.0.28/smartsip-sdk-0.0.28.xcframework.zip",
+            checksum: "2a5dc4671e0d7895f65f61e1399a05f058d2577f8da0e8f079244b33653fead1"
         )
     ]
 )
