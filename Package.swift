41fd8e89078dac392fd66f482bc88376ad68f7b7
Franz Iacob <franz.iacob@cegeka.com>
2026-01-28 13:44:06 +0100

    Release 0.0.53

--- Package.swift
+++ Package.swift
@@ -7,6 +7,6 @@ let package = Package(
     dependencies: [.package(url: "https://gitlab.linphone.org/BC/public/linphone-sdk-swift-ios.git", exact: "5.4.73")],
     targets: [
         .target(name: "SmartSipSDK", dependencies: [.target(name: "SmartSipImplementation"), .product(name: "linphonesw", package: "linphone-sdk-swift-ios")], path: "Sources/SmartSipSDK"),
-        .binaryTarget(name: "SmartSipImplementation", url: "https://github.com/smartconnect-eu/SmartSIP-SDK-Libraries-and-Examples/releases/download/0.0.50/SmartSipImplementation-0.0.50.xcframework.zip", checksum: "168b6d92ae616efd0a2b14cd714ebfa8273a2a57fc034c0bff93c83343fd9ba5")
+        .binaryTarget(name: "SmartSipImplementation", url: "https://github.com/smartconnect-eu/SmartSIP-SDK-Libraries-and-Examples/releases/download/0.0.53/SmartSipImplementation-0.0.53.xcframework.zip", checksum: "b615ca57be6ce09dd71aa4f3d661b72deaae57729c7d5b90f3e03349deb959bd")
     ]
 )
