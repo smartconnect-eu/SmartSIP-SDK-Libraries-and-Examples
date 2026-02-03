13ed7259568be5b36e117977d21f8d2698092c82
Franz Iacob <franz.iacob@cegeka.com>
2026-02-03 10:38:52 +0100

    Update build.gradle.kts

--- Demo/Android/app/build.gradle.kts
+++ Demo/Android/app/build.gradle.kts
@@ -43,7 +43,7 @@ android {
 
 dependencies {
 
-    implementation("cc.smartconnect:smartsip-sdk:0.0.64")
+    implementation("cc.smartconnect:smartsip-sdk:0.0.79")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
