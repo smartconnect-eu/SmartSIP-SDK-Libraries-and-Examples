b67adf106b19efa98051b6ec4d36e3df5f858a11
Franz Iacob <franz.iacob@cegeka.com>
2026-02-20 09:22:45 +0100

    Release 0.0.82 - Public Access

--- Package.swift
+++ Package.swift
@@ -7,6 +7,6 @@ let package = Package(
     dependencies: [.package(url: "https://gitlab.linphone.org/BC/public/linphone-sdk-swift-ios.git", exact: "5.4.73")],
     targets: [
         .target(name: "SmartSipSDK", dependencies: [.target(name: "SmartSipImplementation"), .product(name: "linphonesw", package: "linphone-sdk-swift-ios")], path: "Sources/SmartSipSDK"),
-        .binaryTarget(name: "SmartSipImplementation", url: "https://github.com/smartconnect-eu/SmartSIP-SDK-Libraries-and-Examples/releases/download/0.0.81/SmartSipImplementation-0.0.81.xcframework.zip", checksum: "404fe4312f88f333d36ed247bf8ae25cf06463d910a45c247b9ac9a5c631ca48")
+        .binaryTarget(name: "SmartSipImplementation", url: "https://github.com/smartconnect-eu/SmartSIP-SDK-Libraries-and-Examples/releases/download/0.0.82/SmartSipImplementation-0.0.82.xcframework.zip", checksum: "da6fde6a9cf41f436034375ac6de2ae73fc26335668f7ed36c525c143e8f80f0")
     ]
 )
--- Sources/SmartSipSDK/SmartSipSDK.swift
+++ Sources/SmartSipSDK/SmartSipSDK.swift
@@ -1,5 +1,5 @@
 import Foundation
 public final class SmartSipSDK {
-    public static let sdkVersion = "0.0.81"
+    public static let sdkVersion = "0.0.82"
     public static func setDelegate(_ delegate: Any) {}
 }
