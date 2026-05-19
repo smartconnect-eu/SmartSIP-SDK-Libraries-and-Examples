98feef743d33b44380cbb5d13a952aeb54df504b
Franz Iacob <franz.iacob@cegeka.com>
2026-05-19 12:12:27 +0200

    Release 0.0.85 - Public Access

--- Sources/SmartSipSDK/SmartSipSDK.swift
+++ Sources/SmartSipSDK/SmartSipSDK.swift
@@ -1,5 +1,5 @@
 import Foundation
 public final class SmartSipSDK {
-    public static let sdkVersion = "0.0.83"
+    public static let sdkVersion = "0.0.85"
     public static func setDelegate(_ delegate: Any) {}
 }
---
+++ Sources/linphonesw/module.swift
@@ -0,0 +1,26 @@
+// Re-export Linphone binary modules through the  shim target.
+// This preserves existing SDK imports: .
+
+#if canImport(linphone)
+@_exported import linphone
+#endif
+
+#if canImport(bellesip)
+@_exported import bellesip
+#endif
+
+#if canImport(belcard)
+@_exported import belcard
+#endif
+
+#if canImport(belr)
+@_exported import belr
+#endif
+
+#if canImport(mediastreamer2)
+@_exported import mediastreamer2
+#endif
+
+#if canImport(ortp)
+@_exported import ortp
+#endif
