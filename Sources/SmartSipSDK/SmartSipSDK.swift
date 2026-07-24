60457a2d38f36b6856ca4e2315f696efb44f850b
Franz Iacob <franz.iacob@cegeka.com>
2026-07-24 08:19:51 +0200

    Release 0.1.18 - Public Access

--- Sources/SmartSipSDK/SmartSipSDK.swift
+++ Sources/SmartSipSDK/SmartSipSDK.swift
@@ -1,5 +1,5 @@
 import Foundation
 public final class SmartSipSDK {
-    public static let sdkVersion = "0.1.17"
+    public static let sdkVersion = "0.1.18"
     public static func setDelegate(_ delegate: Any) {}
 }
